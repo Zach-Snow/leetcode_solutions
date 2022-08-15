2e53eaecb90180d532f695844cf1e031df0ba982
Zakir Ayub <ayubzakirtushar@gmail.com>
2022-08-15 21:11:09 +0200

    added valid parentheses solution

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -5,34 +5,28 @@ class Solution:
             "{": "}",
             "[": "]"
         }
-        ending_vals = []
-        step = 0
-        for i in range(len(s)):
-            if (i + step) < len(s):
-                value = s[i+step]
-                for key in bracket_dict:
-                    if value in bracket_dict.keys():
-                        idx = s.index(s[i+step])
-                        try:
-                            followingVal = s[idx+1]
-                            if followingVal == bracket_dict[key]:
-                                step += 1
-                                ending_vals.append(True)
-                            elif followingVal in bracket_dict.keys():
 
-                            else:
-                                ending_vals.append(False)
-                                break
-                        except IndexError:
-                            ending_vals.append(False)
-                            break
-                    elif not value:
-                        pass
-        final_val = list(dict.fromkeys(ending_vals))
-        if len(final_val) == 1:
-            return final_val[0]
+        latest_list = []
+        for char in s:
+            last_pos = len(latest_list) - 1
+            if char not in bracket_dict.keys():
+                try:
+                    if char == bracket_dict[latest_list[last_pos]]:
+                        latest_list.pop(last_pos)
+                    else:
+                        return False
+                except IndexError:
+                    latest_list.append(char)
+                except KeyError:
+                    return False
+            elif char in bracket_dict.keys():
+                latest_list.append(char)
+
+        if not latest_list:
+            return True
         else:
             return False
 
+
 d = Solution()
-print(d.isValid(s="() {]"))
+print(d.isValid(s="))"))
